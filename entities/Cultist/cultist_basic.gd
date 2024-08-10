401d5edd1d72ed79d03a79c6195ae4a0d271aba8
kainosaur <152326772+kainosaur@users.noreply.github.com>
2024-08-10 16:05:31 -0600

    fixed path finding

--- entities/Cultist/cultist_basic.gd
+++ entities/Cultist/cultist_basic.gd
@@ -4,13 +4,8 @@ extends CharacterBody2D
 @onready var player = get_tree().get_nodes_in_group("Player")[0]
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
-var movement_speed: float = 200.0
-
-
-# if player is *in front* of enemy, do attack 
-# right now I will indicate by changing the color
-
-
+var movement_speed: float = 60.0
+var distance_in_front_of_player: float = 200.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,14 +17,23 @@ func _ready():
 func actor_setup():
 	await get_tree().physics_frame
 	
-	set_movement_target(player.position)
+	set_movement_target(Vector2(player.position.x + distance_in_front_of_player, player.position.y))
 
 func set_movement_target(movement_target: Vector2):
 	navigation_agent.target_position = movement_target
 
-func _physics_process(delta):
+#func punch_attack():
+	#
+
+func _physics_process(_delta):
 	if navigation_agent.is_navigation_finished():
+		# Punch!!!
+		
 		return
+	
+	# TODO Set movement target less often 
+	# this could cause performance issues later
+	set_movement_target(Vector2(player.position.x + distance_in_front_of_player, player.position.y))
 	
 	var current_cultist_position: Vector2 = global_position
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
