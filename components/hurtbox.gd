0a849a3f4ca74fc2e3ec88be867e194e129427b5
phorpho <53391100+phorpho@users.noreply.github.com>
2024-08-11 00:24:46 -0400

    COMBO

--- components/hurtbox.gd
+++ components/hurtbox.gd
@@ -1,9 +1,10 @@
 class_name Hurtbox
 extends Area2D
 
-
+#Signal is emitted when a hitbox object in group "enemy" is detected, sends the "damage" value stored in that hitbox object 
 signal received_damage(damage: int)
 
+#Plug in health object, damage will be subtracted from the values stored in this object
 @export var health: Health
 
 func _ready():
@@ -15,7 +16,7 @@ func _on_area_entered(area: Area2D) -> void:
 		if hitbox and hitbox.owner:
 			#Collect hitbox data
 			var damage = hitbox.damage
-			#Apply damage
+			#Apply damage to the health value stored in the export variable
 			if damage > 0:
 				health.health -= hitbox.damage
 				received_damage.emit(hitbox.damage)
---
+++ entities/Player/player_attack1_state.gd
@@ -0,0 +1,36 @@
+extends State
+
+@export var actor: CharacterBody2D
+@export var animator: AnimationPlayer
+
+var combo: bool = false
+
+
+func enter():
+	#Fetch animation and default animation position (for hitbox)
+	var animation = animator.get_animation("attack1")
+	var animation_position = animation.track_get_key_value(0,0)
+	#Check whether the actor is flipped (the CharacterBody2D), if so, negate the animation position
+	check_flip(animation, animation_position)
+	animator.play("attack1")
+	await animator.animation_finished
+	#Reset animation position
+	reset_animation(animation, animation_position)
+	if combo == false:
+		transition.emit(self, "PlayerMoveState")
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("attack2"):
+		combo = true
+		await animator.animation_finished
+		combo = false
+		transition.emit(self, "PlayerAttack2State")
+		
+		
+func reset_animation(animation, position):
+	animation.track_set_key_value(0, 0, position)
+	
+func check_flip(animation, position):
+	if actor.flipped == true:
+		animation.track_set_key_value(0, 0, position * Vector2(-1, 1))
