27a7c2afb9c425e348025c11f3e7cfac19006cd7
shimzini <93698454+shimzini@users.noreply.github.com>
2024-08-15 17:47:56 -0500

    added pause menu

--- scenes/GameScene/Levels/Level1/Level1.gd
+++ scenes/GameScene/Levels/Level1/Level1.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@onready var pause_menu = $PauseMenu
-var pause = false
+@onready var pause_menu = $Player/CanvasLayer/PlayerUi/PauseMenu
+var paused = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,6 +9,19 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("enter"):
+		pause()
+	
+func pause():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+		paused = !paused
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+		paused = !paused
+		
+
 	
 		
--- scenes/Menus/PauseMenu/PauseMenu.gd
+++ scenes/Menus/PauseMenu/PauseMenu.gd
@@ -1,1 +1,25 @@
-extends "res://addons/maaacks_game_template/extras/scenes/PauseMenu/PauseMenu.gd"
+extends Control
+
+@onready var level = $"../../../.."
+# Called when the node enters the scene tree for the first time.
+
+func _on_restart_pressed():
+	level.pause()
+	get_tree().reload_current_scene()
+	
+
+
+
+func _on_resume_pressed():
+	level.pause()
+
+
+
+func _on_main_menu_pressed():
+	#get_tree().change_scene_to_file()
+	pass
+	
+
+
+func _on_quit_pressed():
+	get_tree().quit()
